919da09eac48b3634842e0833066fd6b4f695232
Greg Einfrank <greg.einfrank@gmail.com>
2020-07-15 08:00:41 -0700

    span

--- background.js
+++ background.js
@@ -2,7 +2,7 @@ chrome.commands.onCommand.addListener(function(command) {
   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
     chrome.tabs.executeScript(
         tabs[0].id,
-        {code: 'document.querySelectorAll(".SingleTaskPaneSpreadsheet a.TaskStoryFeed-expandLink").forEach(a => a.click())'});
+        {code: 'document.querySelectorAll(".SingleTaskPaneSpreadsheet span.TaskStoryFeed-expandLink").forEach(a => a.click())'});
     chrome.tabs.executeScript(
         tabs[0].id,
         {code: 'document.querySelectorAll(".SingleTaskPaneSpreadsheet span.TruncatedRichText-expand").forEach(a => a.click())'});
