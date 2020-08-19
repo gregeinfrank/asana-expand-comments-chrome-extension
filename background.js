c169f314d10f00f19e4bda6dd21d064f5562a150
Greg Einfrank <greg.einfrank@gmail.com>
2020-08-19 12:35:19 -0700

    Update for conversations

--- background.js
+++ background.js
@@ -6,5 +6,12 @@ chrome.commands.onCommand.addListener(function(command) {
     chrome.tabs.executeScript(
         tabs[0].id,
         {code: 'document.querySelectorAll(".SingleTaskPaneSpreadsheet span.TruncatedRichText-expand").forEach(a => a.click())'});
+
+    chrome.tabs.executeScript(
+        tabs[0].id,
+        {code: 'document.querySelectorAll(".ConversationPane span.ConversationStoryFeed-expandLink").forEach(a => a.click())'});
+    chrome.tabs.executeScript(
+        tabs[0].id,
+        {code: 'document.querySelectorAll(".ConversationPane span.TruncatedRichText-expand").forEach(a => a.click())'});
   });
 });
